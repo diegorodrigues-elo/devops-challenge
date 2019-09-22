5b516acb0766daaaf6d7daf8286a582c5754d99a
Leonardo Barboza Dias <inmetrics_lbdias@orizon.com.br>
2019-09-22 15:03:06 -0300

    tentando rodar como conteiner

--- app/application.py
+++ app/application.py
@@ -6,7 +6,7 @@ key = 'ELO_PASS'
 from flask import Flask
 from flask_httpauth import HTTPTokenAuth
 
-app = Flask(__name__)
+application = Flask(__name__)
 auth = HTTPTokenAuth("Token")
 
 
@@ -14,10 +14,10 @@ auth = HTTPTokenAuth("Token")
 def verify_token(token):
     return token == os.getenv(key)
 
-@app.route("/")
+@application.route("/")
 @auth.login_required
 def main_route():
    return "devops test server flying!!"
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=80)
+    application.run("0.0.0.0", port=80)
